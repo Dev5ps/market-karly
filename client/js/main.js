19ddddd987b51b8656206e72d77d21f64cdb7fc5
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-02-06 02:26:41 +0900

    [feat] 장바구니 페이지 상품 전체 선택 기능 추가 #75

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,4 @@
-import { toggleClassAction, listNavigationToggle, rendingProductItem } from "./index.js";
+import { toggleClassAction, listNavigationToggle, rendingProductItem, selectAll } from "./index.js";
 
 /* 네비게이션 카테고리 드롭다운 토글 기능 */
 toggleClassAction('.category', '.navigation .dropdown', 'is-active');
@@ -8,6 +8,10 @@ toggleClassAction('.category', '.navigation .dropdown', 'is-active');
 toggleClassAction('.refrigerated-food-container', '.refrigerated-food .item', 'is-active');
 toggleClassAction('.frozen-food-container', '.frozen-food .item', 'is-active');
 toggleClassAction('.room-temperature-food-container', '.room-temperature-food .item', 'is-active');
+/* -------------------------------------------------------------------------- */
+
+/* 장바구니 페이지 상품 전체 선택 기능 */
+selectAll('.main-cart .item-checkbox', '.main-cart .select__checkbox', '.main-cart .item-checkbox, .main-cart .select__checkbox');
 /* -------------------------------------------------------------------------- */
 
 rendingProductItem('.product-list .list-contents .list');
