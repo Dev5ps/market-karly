e4eb31d3fc79b1d79124eee79b597ef6b009233e
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-02-05 21:30:14 +0900

    [feat] 장바구니 페이지 보관 방식 별 아이템 마크업 추가 및 탭 토글 기능 추가 #73

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,14 @@
 import { toggleClassAction, listNavigationToggle, rendingProductItem } from "./index.js";
 
+/* 네비게이션 카테고리 드롭다운 토글 기능 */
 toggleClassAction('.category', '.navigation .dropdown', 'is-active');
+/* -------------------------------------------------------------------------- */
+
+/* 장바구니 페이지 상품 탭 토글 기능 */
+toggleClassAction('.refrigerated-food-container', '.refrigerated-food .item', 'is-active');
+toggleClassAction('.frozen-food-container', '.frozen-food .item', 'is-active');
+toggleClassAction('.room-temperature-food-container', '.room-temperature-food .item', 'is-active');
+/* -------------------------------------------------------------------------- */
 
 rendingProductItem('.product-list .list-contents .list');
 listNavigationToggle('.product-list .list-navigation');
