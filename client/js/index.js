32deedbe17deb8e95b9deb32dee7d9c133bd137a
Jihoon <jangna96@gmail.com>
2023-02-06 10:23:57 +0900

    [feat] 메인페이지 제품 리스트 ajax 데이터 통신 기능 추가 #80

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,4 @@
 export * from './common/index.js';
 export * from './product-list/index.js';
+export * from './main/index.js';
 export * from '../lib/index.js';
--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,10 @@
-import { toggleClassAction, listNavigationToggle, rendingProductItem } from "./index.js";
+import { toggleClassAction, listNavigationToggle, rendingProductItem, rendingMainProductItem } from "./index.js";
 
 toggleClassAction('.category', '.navigation .dropdown', 'is-active');
 
 rendingProductItem('.product-list .list-contents .list');
 listNavigationToggle('.product-list .list-navigation');
+
+
+rendingMainProductItem('.main-product-list-first .swiper-wrapper');
+rendingMainProductItem('.main-product-list-second .swiper-wrapper');
