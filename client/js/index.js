b3844c45b8240433f56fa962497cafcf1e75bc41
ChulJin Lim <cjfwls9806@gmail.com>
2023-02-08 11:49:29 +0900

    [feat] main.js 기능 추가 및 data.json 관리자 추가 #103

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,5 @@
 export * from './common/index.js';
 export * from './product-list/index.js';
 export * from '../lib/index.js';
+export * from './register/index.js';
+export * from './login/index.js'
--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,9 @@
-import { toggleClassAction, listNavigationToggle, rendingProductItem } from "./index.js";
+import { toggleClassAction, listNavigationToggle, rendingProductItem, loginProgress } from "./index.js";
+import { registerProgress } from "./register/register.js";
 
 toggleClassAction('.category', '.navigation .dropdown', 'is-active');
 
 rendingProductItem('.product-list .list-contents .list');
 listNavigationToggle('.product-list .list-navigation');
+registerProgress();
+loginProgress();
